dd5db5bacb1cc4a9b0d7cc6b28409e984ec6ad96
Wilson Birch <wilsonbirch@gmail.com>
2024-12-13 23:07:49 -0500

    updated page title

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "birAI"
+const APP_DEFAULT_TITLE = "birAI"
+const APP_TITLE_TEMPLATE = "%s - birAI"
+const APP_DESCRIPTION = "birAI UI PWA!"
 
 interface RootLayoutProps {
   children: ReactNode
